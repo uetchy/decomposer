462cfd2a0a4d01da1d8f69e6bd819d217b07a10e
Yasuaki Uechi <uetchy@randompaper.co>
2015-02-26 19:47:17 +0900

    Add test

--- test/decomposer.test.js
+++ test/decomposer.test.js
@@ -1,10 +1,20 @@
-var Decomposer = require('../decomposer.js')
+var expect = require('chai').expect,
+    through = require('through2'),
+    path = require('path'),
+    fs = require('fs'),
+    gulp = require('gulp'),
+    decomposer = require('../decomposer.js')
 
-describe('Decomposer', function() {
+describe('decomposer', function() {
+  var filename = path.join(__dirname, './fixtures/index.sass');
+
   it('can create instance', function(done) {
-    console.log(typeof(readLines));
-    decomposer = Decomposer.parse('test/test.sass', function(){
-      done();
-    });
+    gulp.src(filename)
+      .pipe(decomposer())
+      .pipe(through.obj(function(file, enc, cb) {
+        var expected = String(fs.readFileSync(path.join(__dirname, './fixtures/index.sass.expected')));
+        expect(String(file.contents)).to.be.equal(expected);
+        done();
+      }));
   });
 });
